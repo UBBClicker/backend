13dc9b4175cab97837ee091b4e54dd4818fff746
Osc44r <oski90091@gmail.com>
2025-03-02 16:54:16 +0100

    Created MongoDB connection

--- app/config.py
+++ app/config.py
@@ -8,6 +8,9 @@ load_dotenv(find_dotenv())
 class Config(BaseSettings):
     APP_NAME: str = Field("UBBClicker", description="The name of the app")
 
+    MONGO_DATABASE: str = Field("UBBClicker", description="The name of the database")
+    MONGO_URI: str = Field("mongodb://localhost:27017/", description="The URI of the MongoDB database")
+   
     PASSWORD_TOKEN: str
 
 
---
+++ app/mongodb/connection.py
@@ -0,0 +1,66 @@
+from typing import Optional
+
+from motor import motor_asyncio, core
+from odmantic import AIOEngine
+from pymongo.driver_info import DriverInfo
+
+from ..config import config
+
+
+class MongoConnection:
+    _instance: Optional["MongoConnection"] = None
+
+    def __init__(
+            self,
+            mongo_uri: str,
+            database: str,
+            driver_info: DriverInfo,
+            *,
+            connect_timeout: Optional[int] = 5000,
+    ):
+        if MongoConnection._instance is not None:
+            raise RuntimeError("Use 'get_connection' to access the MongoConnection instance.")
+
+        self.client = motor_asyncio.AsyncIOMotorClient(
+            mongo_uri,
+            driver=driver_info,
+            serverSelectionTimeoutMS=connect_timeout,
+        )
+        self.engine = AIOEngine(client=self.client, database=database)
+
+    def get_database(self) -> core.AgnosticDatabase:
+        return self.client[self.engine.database.name]
+
+    async def ping_database(self):
+        await self.get_database().command("ping")
+
+    @classmethod
+    def get_connection(
+            cls,
+            mongo_uri: str,
+            database: str,
+            driver_info: DriverInfo,
+            *,
+            connect_timeout: Optional[int] = None
+    ) -> "MongoConnection":
+        if cls._instance is None:
+            cls._instance = cls(
+                mongo_uri=mongo_uri,
+                database=database,
+                driver_info=driver_info,
+                connect_timeout=connect_timeout
+            )
+        return cls._instance
+
+    @classmethod
+    def close_connection(cls):
+        if cls._instance is not None:
+            cls._instance.client.close()
+            cls._instance = None
+
+
+connection = MongoConnection.get_connection(
+    config.MONGO_URI, config.MONGO_DATABASE, DriverInfo(config.APP_NAME)
+)
+
+__all__ = ["connection"]
