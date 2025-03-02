cb2023ba318d1babe4b0bc6c6c756e538981a249
Osc44r <oski90091@gmail.com>
2025-03-02 18:37:48 +0100

    Added unique index for user's nickname

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,12 @@
-from odmantic import Model, Field
+from odmantic import Model, Field, Index
 
 
 class User(Model):
     nickname: str = Field(..., description="The user's nickname")
     password: str = Field(..., description="The user's password")
+
+    model_config = {
+        "indexes": lambda: [
+            Index(User.nickname, unique=True),
+        ]
+    }
